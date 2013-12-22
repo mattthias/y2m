0947864cbab3aa192c19999e5dfc3edbeb7ee186
Matthias Schmitz <matthias@sigxcpu.org>
2013-12-22 09:57:00 +0100

    Add progress bar

--- y2m.sh
+++ y2m.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -ex
+#!/bin/sh
+
+MP3_DIR="$HOME/Musik"
 
 AVCONV_CMD="avconv -i %f -ab 192k -acodec mp3 mp3/%n.mp3"
 REMOVE_SRC_CMD="rm %f"
 
 URL=$(zenity --title "Youtube URL to download" --entry --text "Youtube URL eingeben")
 
-cclive --tr "/(\w)/g"  --exec "$AVCONV_CMD" --exec "$REMOVE_SRC_CMD" $URL
+cclive --tr "/(\w)/g"  --exec "$AVCONV_CMD" --exec "$REMOVE_SRC_CMD" $URL 2>&1 | zenity --progress --text "Download and conversion in progress" --auto-close --pulsate
