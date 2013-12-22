ce688b0986a3ed63873f61f60a5192ff65a7107a
Matthias Schmitz <matthias@sigxcpu.org>
2013-12-22 09:59:45 +0100

    Use mp3 output dir

--- y2m.sh
+++ y2m.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+# Change default save path for mp3 files
 MP3_DIR="$HOME/Musik"
 
-AVCONV_CMD="avconv -i %f -ab 192k -acodec mp3 mp3/%n.mp3"
+AVCONV_CMD="avconv -i %f -ab 192k -acodec mp3 $MP3_DIR/%n.mp3"
 REMOVE_SRC_CMD="rm %f"
 
-URL=$(zenity --title "Youtube URL to download" --entry --text "Youtube URL eingeben")
+URL=$(zenity --title "Youtube URL to download" --entry --text "Enter Youtube URL")
 
 cclive --tr "/(\w)/g"  --exec "$AVCONV_CMD" --exec "$REMOVE_SRC_CMD" $URL 2>&1 | zenity --progress --text "Download and conversion in progress" --auto-close --pulsate
